f30ce2e8a0ee96f6a1b25d044016bb1ad898da49
Sergey Avseyev <sergey.avseyev@gmail.com>
2014-03-02 22:42:42 +0300

    Allow to check for support of given URL

--- ext/quvi_ext/quvi_ext.c
+++ ext/quvi_ext/quvi_ext.c
@@ -11,6 +11,13 @@ VALUE qv_mQuvi;
 VALUE qv_cHandle;
 VALUE qv_eError;
 
+ID qv_sym_any;
+ID qv_sym_media;
+ID qv_sym_online;
+ID qv_sym_playlist;
+ID qv_sym_subtitle;
+ID qv_sym_type;
+
 typedef struct qv_handle_st
 {
     quvi_t q;
@@ -76,11 +83,59 @@ VALUE qv_handle_init(int argc, VALUE *argv, VALUE self)
     return self;
 }
 
+VALUE qv_handle_supports_p(int argc, VALUE *argv, VALUE self)
+{
+    qv_handle_t *handle = DATA_PTR(self);
+    VALUE url, opts;
+    QuviSupportsType type = QUVI_SUPPORTS_TYPE_ANY;
+    QuviSupportsMode mode = QUVI_SUPPORTS_MODE_OFFLINE;
+    QuviBoolean r;
+
+    rb_scan_args(argc, argv, "11", &url, &opts);
+    Check_Type(url, T_STRING);
+    if (!NIL_P(opts)) {
+        VALUE arg;
+        Check_Type(opts, T_HASH);
+        arg = rb_hash_lookup2(opts, qv_sym_online, Qundef);
+        if (arg != Qundef) {
+            mode = RTEST(arg) ? QUVI_SUPPORTS_MODE_ONLINE : QUVI_SUPPORTS_MODE_OFFLINE;
+        }
+        arg = rb_hash_aref(opts, qv_sym_type);
+        if (arg != Qnil) {
+            if (arg == qv_sym_media) {
+                type = QUVI_SUPPORTS_TYPE_MEDIA;
+            } else if (arg == qv_sym_playlist) {
+                type = QUVI_SUPPORTS_TYPE_PLAYLIST;
+            } else if (arg == qv_sym_subtitle) {
+                type = QUVI_SUPPORTS_TYPE_SUBTITLE;
+            } else if (arg == qv_sym_any) {
+                type = QUVI_SUPPORTS_TYPE_ANY;
+            } else {
+                rb_raise(rb_eArgError, "unknown type: %s", StringValuePtr(arg));
+            }
+        }
+    }
+
+    r = quvi_supports(handle->q, RSTRING_PTR(url), mode, type);
+    return r == QUVI_TRUE ? Qtrue : Qfalse;
+}
+
 void init_quvi_handle()
 {
     qv_cHandle = rb_define_class_under(qv_mQuvi, "Handle", rb_cObject);
     rb_define_alloc_func(qv_cHandle, qv_handle_alloc);
     rb_define_method(qv_cHandle, "initialize", qv_handle_init, -1);
+    rb_define_method(qv_cHandle, "supports?", qv_handle_supports_p, -1);
+}
+
+void init_symbols()
+{
+    qv_sym_any = ID2SYM(rb_intern("any"));
+    qv_sym_media = ID2SYM(rb_intern("media"));
+    qv_sym_online = ID2SYM(rb_intern("online"));
+    qv_sym_playlist = ID2SYM(rb_intern("playlist"));
+    qv_sym_subtitle = ID2SYM(rb_intern("subtitle"));
+    qv_sym_type = ID2SYM(rb_intern("type"));
 }
 
 void Init_quvi_ext()
@@ -97,5 +152,7 @@ void Init_quvi_ext()
     rb_define_const(qv_mVersion, "QUVI_BUILD_TIME", rb_str_freeze(rb_external_str_new_cstr(quvi_version(QUVI_VERSION_BUILD_TIME))));
 
     qv_eError = rb_const_get(qv_mQuvi, rb_intern("Error"));
+
+    init_symbols();
     init_quvi_handle();
 }
