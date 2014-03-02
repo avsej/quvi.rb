f79d7004e2fbda3e514c0f401132197299efe324
Sergey Avseyev <sergey.avseyev@gmail.com>
2014-03-02 19:59:45 +0300

    Convert version to hash

--- lib/quvi/version.rb
+++ lib/quvi/version.rb
@@ -3,11 +3,29 @@ module Quvi
     MAJOR = 0
     MINOR = 0
     PATCH = 1
+    FULL = [MAJOR, MINOR, PATCH].join(".").freeze
 
     def self.to_str
-      [MAJOR, MINOR, PATCH].join(".")
+      FULL
     end
-    class << self; alias to_s to_str; end
+
+    def self.to_hash
+      @version_hash ||= begin
+                          require 'quvi_ext'
+                          {
+                            version: to_s,
+                            quvi_version: QUVI_VERSION,
+                            quvi_configuration: QUVI_CONFIGURATION,
+                            quvi_build_cc_cflags: QUVI_BUILD_CC_CFLAGS,
+                            quvi_build_target: QUVI_BUILD_TARGET,
+                            quvi_build_time: QUVI_BUILD_TIME
+                          }.freeze
+                        end
+    end
+    class << self
+      alias to_s to_str
+      alias to_h to_hash
+    end
 
     def self.inspect
       to_s.inspect
