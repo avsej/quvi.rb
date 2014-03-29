ec81f74f5908cf46864804fb31bd150c1c350a4f
Sergey Avseyev <sergey.avseyev@gmail.com>
2014-03-29 23:06:37 +0300

    remove duplicated attribute

--- lib/quvi/handle.rb
+++ lib/quvi/handle.rb
@@ -2,6 +2,6 @@ module Quvi
   class Handle
     DEFAULT_USER_AGENT = "Mozilla/5.0".freeze
 
-    attr_accessor :user_agent, :allow_cookies, :user_agent
+    attr_accessor :user_agent, :allow_cookies
   end
 end
